15fd7d013fa60571874a7c5b97c0a102a83fa3f4
kazutolure <64045767+Kazuto-neves@users.noreply.github.com>
2021-06-07 14:45:53 -0300

    Update IAlugavel.h

--- include/IAlugavel.h
+++ include/IAlugavel.h
@@ -8,11 +8,13 @@ class IAlugavel{
     public:
         virtual void ler()                 = 0;
         virtual int getCodigo()            = 0;
+        virtual string getTitulo()         = 0;
         virtual string getClasse()         = 0;
         virtual string getEstilo()         = 0;
         virtual string getProdutora()      = 0;
         virtual string getMidia()          = 0;
-        virtual int getAno()               = 0;   
+        virtual int getAno()               = 0;  
+        virtual double aluguel()           = 0; 
 };
 
 #endif
